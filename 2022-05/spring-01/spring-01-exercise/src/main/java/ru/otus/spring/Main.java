ebe1dcb848ec6a2f649c94362748c366896a096d
Wadim <SalnikovVN@alfastrah.ru>
2022-06-14 17:18:50 +0300

    dz 1

--- 2022-05/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2022-05/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
@@ -1,17 +1,18 @@
 package ru.otus.spring;
 
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.otus.spring.domain.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.spring.service.QuestsService;
 
 public class Main {
 
     public static void main(String[] args) {
-        // TODO: создайте здесь класс контекста
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        QuestsService service = context.getBean(QuestsService.class);
+        var ivan = service.getQuest();
+        ivan.forEach(i->{
+            System.out.println("вопрос = " + i.quest);
+        });
 
-        // TODO: Получите Person Service
-
-        // Получите Person "Ivan"
-        Person ivan = null;
-        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+        context.close();
     }
 }
